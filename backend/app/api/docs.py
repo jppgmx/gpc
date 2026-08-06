d1cb9d9427fc8c6f0307b6a2309129258caacc5f
jppgmx <74308134+jppgmx@users.noreply.github.com>
2026-08-06 20:11:30 -0300

    Adicionar endpoints para documentação

---
+++ backend/app/api/docs.py
@@ -0,0 +1,112 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import PlainTextResponse
+
+from services.data_store import DataStore
+from services.document_provider import DocumentProvider
+from models.sources.url import URLSource
+from models.sources.cache import CacheSource
+from models.transformers.html_transformer import ContentHTMLTransformer
+from models.transformers.markdown import HTML2MarkdownTransformer
+from models.mdconvs.privacy import PrivacyNormalizer
+
+router = APIRouter()
+data_store = DataStore()
+provider = DocumentProvider(data_store=data_store)
+
+data_store.allocate_dir("docs")
+provider.add_source(
+    source=URLSource(
+        document_id="terms",
+        url="https://codeforces.com/terms",
+        # O foco é normalizar o HTML para o resultado ser Markdown
+        transformers=[
+            ContentHTMLTransformer(element="#pageContent"),
+            HTML2MarkdownTransformer(
+                converter_options={
+                    "wrap": True,
+                    "wrap_width": 100
+                }
+            )
+        ],
+        cache_lifetime=3600, # 1 hora,
+        cached_file=data_store.allocate_file("docs", "terms.md"),
+        data_store=data_store,
+
+        # Eu particularmente achei algo muito estranho:
+        # O site do Codeforces é protegido pelo Cloudflare, e obviamente com os seus desafios de bot,
+        # então se você fizer uma requisição simples, ele vai bloquear e retornar um erro 403.
+        # Então, para contornar isso, eu adicionei um User-Agent de um navegador e por incrível que
+        # pareça, funciona. Apliquei no curl, também. Mas eu que uso PowerShell, algo faz com que o .NET
+        # não funciona. Eu não entendo a natureza do Cloudflare.
+
+        # Eu entendo que isso é algo que não é ideal, pois o Cloudflare faz isso para evitar scrapping,
+        # mas note que o scrapping que eu faço não tem más intenções.
+        request_options={
+            "headers": {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                "(KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36 Edg/151.0.0.0"
+            }
+        }
+    )
+)
+
+provider.add_source(
+    source=URLSource(
+        document_id="privacy",
+        url="https://codeforces.com/privacy",
+        transformers=[
+            ContentHTMLTransformer(element="#pageContent"),
+            HTML2MarkdownTransformer(
+                converter_cls=PrivacyNormalizer, 
+                converter_options={
+                    "wrap": True
+                }
+            )
+        ],
+        cache_lifetime=3600, # 1 hora,
+        cached_file=data_store.allocate_file("docs", "privacy.md"),
+        data_store=data_store,
+        request_options={
+            "headers": {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                "(KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36 Edg/151.0.0.0"
+            }
+        }
+    )
+)
+
+provider.add_source(
+    source=CacheSource(
+        document_id="disclaimer",
+        cached_file=data_store.allocate_file(".assets", "docs", "disclaimer.md"),
+        cache_lifetime=None,
+        data_store=data_store
+    )
+)
+
+@router.get("/terms", response_class=PlainTextResponse)
+def get_terms():
+    try:
+        document = provider.fetch_document("terms")
+
+        return document.text
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
+
+@router.get("/privacy", response_class=PlainTextResponse)
+def get_privacy():
+    try:
+        document = provider.fetch_document("privacy")
+
+        return document.text
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
+
+@router.get("/disclaimer", response_class=PlainTextResponse)
+def get_disclaimer():
+    try:
+        document = provider.fetch_document("disclaimer")
+
+        return document.text
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
