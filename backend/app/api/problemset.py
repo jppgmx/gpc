a537b570b099e44ff4816def16c07b4971c6ec39
jppgmx <74308134+jppgmx@users.noreply.github.com>
2026-08-14 19:48:28 -0300

    Adicionar endpoints /api/problemset

---
+++ backend/app/api/problemset.py
@@ -0,0 +1,245 @@
+from re import match
+from typing import Literal, Optional, Annotated, get_args
+
+from fastapi import APIRouter, Query
+from pydantic import BaseModel, Field, ConfigDict, field_validator
+
+from services.db import get_db_session
+from models.problemset import Problem
+
+# Regex para identificar a contest e problema
+PROBLEM_ID_REGEX = r"^(?P<contest>[0-9]+)(?P<problem>[A-Z][0-9]?)$"
+
+router = APIRouter(prefix="/problemset", tags=["Problemset"])
+
+type Order = Literal["contestId", "index", "solvedCount", "rating", "points", "name"]
+
+class FilterParams(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+    q: Optional[str] = Field(
+        None,
+        description="Termo de pesquisa para o nome do problema",
+    )
+    contest_id: Optional[int] = Field(
+        None,
+        description="ID da contest"
+    )
+    index: Optional[str] = Field(
+        None,
+        description="ID do problema",
+        pattern=r"^[A-Z][0-9]?$"
+    )
+    rating: Optional[int] = Field(
+        None,
+        description="Rating do problema",
+    )
+    min_points: Optional[int] = Field(
+        None,
+        description="Número mínimo de pontos do problema",
+    )
+    tags: Optional[str] = Field(
+        None,
+        description="Lista de tags do problema separados por vírgula.",
+    )
+    order_by: Optional[str] = Field(
+        "name",
+        description="Campo para ordenação com vários fatores, separados por vírgula." \
+        " Campos válidos: contestId, index, solvedCount, rating, points, name. " \
+        "Prefixo - em cada para inverter a ordem",
+    )
+    limit: Optional[int] = Field(
+        10,
+        description="Número máximo de problemas a serem retornados",
+        ge=1,
+        le=100
+    )
+    page: Optional[int] = Field(
+        1,
+        description="Número da página de resultados",
+        ge=1
+    )
+
+    @property
+    def tags_list(self) -> Optional[list[str]]:
+        if self.tags is None:
+            return None
+
+        # Remove espaços e divide por vírgula
+        normalized = [tag.strip() for tag in self.tags.split(",") if tag.strip()]
+        if len(normalized) == 0:
+            return None
+
+        return normalized
+
+    @property
+    def order_by_list(self) -> list[str]:
+        if self.order_by is None:
+            return ["name"]
+
+        # Remove espaços e divide por vírgula
+        normalized = [order.strip() for order in self.order_by.split(",") if order.strip()]
+        if len(normalized) == 0:
+            return ["name"]
+
+        # Valida se todos os campos são válidos
+        valid_fields = get_args(Order.__value__)
+        for order in normalized:
+            field_name = order[1:] if order.startswith("-") else order
+            if field_name not in valid_fields:
+                raise ValueError(f"Campo inválido para ordenação: {field_name}")
+
+        return normalized
+
+    @field_validator("tags", mode="before")
+    @classmethod
+    def validate_tags(cls, v):
+        if v is None:
+            return v
+
+        # Remove espaços e divide por vírgula
+        normalized = [tag.strip() for tag in v.split(",") if tag.strip()]
+        if len(normalized) == 0:
+            return None
+
+        return v
+
+    @field_validator("order_by", mode="before")
+    @classmethod
+    def validate_order_by(cls, v):
+        if v is None:
+            return ["name"]
+
+        # Remove espaços e divide por vírgula
+        normalized = [order.strip() for order in v.split(",") if order.strip()]
+        if len(normalized) == 0:
+            return ["name"]
+
+        # Valida se todos os campos são válidos
+        valid_fields = get_args(Order.__value__)
+        for order in normalized:
+            field_name = order[1:] if order.startswith("-") else order
+            if field_name not in valid_fields:
+                raise ValueError(f"Campo inválido para ordenação: {field_name}")
+
+        return v
+
+class ProblemResponse(BaseModel):
+    model_config = ConfigDict(extra="allow")
+
+    total: int
+    page: int
+    limit: int
+    problems: list[dict]
+
+EMPTY_RESPONSE = ProblemResponse(total=0, page=1, limit=10, problems=[])
+def single(problem: Problem) -> ProblemResponse:
+    return ProblemResponse(total=1, page=1, limit=1, problems=[to_dict(problem)])
+
+def is_valid_page(page: int, limit: int, total: int) -> bool:
+    """ Verifica se a página solicitada é válida """
+    return (page - 1) * limit < total
+
+@router.get("/problems")
+def get_problems(filter: Annotated[FilterParams, Query()]):
+    with get_db_session() as session:
+        query = session.query(Problem)
+
+        if filter.contest_id and filter.index:
+            # Estamos fazendo o mesmo que /problems/{problem_id}, então podemos retornar apenas um problema
+            problem = query.filter_by(contest_id=filter.contest_id, index=filter.index).first()
+            if not problem:
+                return EMPTY_RESPONSE
+
+            return single(problem)
+
+        if filter.q:
+            query = query.filter(Problem.name.ilike(f"%{filter.q}%"))
+        if filter.contest_id:
+            query = query.filter(Problem.contest_id == filter.contest_id)
+        if filter.index:
+            query = query.filter(Problem.index == filter.index)
+        if filter.rating:
+            query = query.filter(Problem.rating == filter.rating)
+        if filter.min_points:
+            query = query.filter(Problem.points >= filter.min_points)
+        if filter.tags:
+            for tag in filter.tags_list:
+                query = query.filter(Problem.tags.any(name=tag))
+
+        total_count = query.count()
+
+        if not is_valid_page(filter.page, filter.limit, total_count):
+            return ProblemResponse(
+                error="Página inválida",
+                total=total_count,
+                page=filter.page,
+                limit=filter.limit,
+                problems=[]
+            )
+
+        ORDER_MAPPINGS = {
+            "contestId": "contest_id",
+            "index": "index",
+            "solvedCount": "solved_count",
+            "rating": "rating",
+            "points": "points",
+            "name": "name"
+        }
+        for order in filter.order_by_list:
+            mapped_order = ORDER_MAPPINGS.get(order.lstrip("-"))
+            if order.startswith("-"):
+                query = query.order_by(getattr(Problem, mapped_order).desc())
+            else:
+                query = query.order_by(getattr(Problem, mapped_order))
+
+        problems = (
+            query.offset((filter.page - 1) * filter.limit)
+            .limit(filter.limit)
+            .all()
+        )
+
+        return ProblemResponse(
+            total=total_count,
+            page=filter.page,
+            limit=filter.limit,
+            problems=[to_dict(problem) for problem in problems]
+        )
+
+@router.get("/problems/{problem_id}")
+def get_problem(problem_id: Annotated[str, Field(pattern=PROBLEM_ID_REGEX)]):
+    """
+        Retorna informações sobre um problema específico.
+    """
+
+    contest_id, problem_index = match(PROBLEM_ID_REGEX, problem_id).groups()
+    result = {}
+    with get_db_session() as session:
+        problem = session.query(Problem).filter_by(contest_id=contest_id, index=problem_index).first()
+
+        if not problem:
+            return {
+                "error": "Problema não encontrado."
+            }
+
+        result = to_dict(problem)
+
+    return result
+
+def to_dict(problem: Problem) -> dict:
+    result = {}
+
+    result['contestId'] = problem.contest_id
+    if problem.problemset_name:
+        result['problemsetName'] = problem.problemset_name
+    result['index'] = problem.index
+    result['name'] = problem.name
+    result['type'] = problem.type.value
+    if problem.points is not None:
+        result['points'] = problem.points
+    if problem.rating is not None:
+        result['rating'] = problem.rating
+    result['solvedCount'] = problem.solved_count
+    result['tags'] = [tag.name for tag in problem.tags]
+
+    return result
