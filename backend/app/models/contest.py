7d385005326a5268b4a97b43351d695940990c17
jppgmx <74308134+jppgmx@users.noreply.github.com>
2026-08-14 14:32:00 -0300

    Adicionar models para Problem e Contest do Codeforces

---
+++ backend/app/models/contest.py
@@ -0,0 +1,82 @@
+"""
+    Módulo do model de concursos (contests) do Codeforces
+
+    A modelagem final dos problemas foi determinada da seguinte forma:
+    - Documentação oficial do Codeforces: https://codeforces.com/apiHelp
+    - A realidade vinda dos testes
+
+    Dado uma amostra de 11356 problemas, eis a frequência de cada campo:
+
+    - id = 2139/2139 (100,00%)
+    - name = 2139/2139 (100,00%)
+    - type = 2139/2139 (100,00%)
+    - phase = 2139/2139 (100,00%)
+    - frozen = 2139/2139 (100,00%)
+    - durationSeconds = 2139/2139 (100,00%)
+    - freezeDurationSeconds = 14/2139 (0,65%)
+    - startTimeSeconds = 2139/2139 (100,00%)
+    - relativeTimeSeconds = 2139/2139 (100,00%)
+    - preparedBy = 0/2139 (0,00%)
+    - websiteUrl = 0/2139 (0,00%)
+    - description = 0/2139 (0,00%)
+    - difficulty = 0/2139 (0,00%)
+    - kind = 0/2139 (0,00%)
+    - icpcRegion = 0/2139 (0,00%)
+    - country = 0/2139 (0,00%)
+    - city = 0/2139 (0,00%)
+    - season = 0/2139 (0,00%)
+
+    Aqui o resultado é curioso, pois a maioria documentada não é usada, além de freezeDurationSeconds
+    ter apenas 14 ocorrências. Mas assim como os problemas, vamos manter compatibilidade com a
+    documentação oficial.
+"""
+
+import enum
+from typing import Optional
+from sqlalchemy import Text, Integer, Enum
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+class ContestsBase(DeclarativeBase):
+    """ Base do banco de concursos (contests) do Codeforces """
+
+class ContestType(enum.Enum):
+    """ Tipos de concursos """
+
+    CF = 'CF'
+    IOI = 'IOI'
+    ICPC = 'ICPC'
+
+class ContestPhase(enum.Enum):
+    """ Fases de concursos """
+
+    BEFORE = 'BEFORE'
+    CODING = 'CODING'
+    PENDING_SYSTEM_TEST = 'PENDING_SYSTEM_TEST'
+    SYSTEM_TEST = 'SYSTEM_TEST'
+    FINISHED = 'FINISHED'
+
+class Contest(ContestsBase):
+    """ Representa um concurso (contest) do Codeforces """
+
+    __tablename__ = 'contest'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(Text)
+    type: Mapped[ContestType] = mapped_column(Enum(ContestType))
+    phase: Mapped[ContestPhase] = mapped_column(Enum(ContestPhase))
+    frozen: Mapped[bool]
+    duration_seconds: Mapped[int] = mapped_column("durationSeconds", Integer)
+    freeze_duration_seconds: Mapped[Optional[int]] = mapped_column("freezeDurationSeconds", Integer, nullable=True)
+    start_time_seconds: Mapped[Optional[int]] = mapped_column("startTimeSeconds", Integer, nullable=True)
+    relative_time_seconds: Mapped[Optional[int]] = mapped_column("relativeTimeSeconds", Integer, nullable=True)
+    prepared_by: Mapped[Optional[str]] = mapped_column("preparedBy", Text, nullable=True)
+    website_url: Mapped[Optional[str]] = mapped_column("websiteUrl", Text, nullable=True)
+    description: Mapped[Optional[str]] = mapped_column(Text(), nullable=True)
+    difficulty: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    kind: Mapped[Optional[str]] = mapped_column(Text(), nullable=True)
+    icpc_region: Mapped[Optional[str]] = mapped_column("icpcRegion", Text(), nullable=True)
+    country: Mapped[Optional[str]] = mapped_column(Text(), nullable=True)
+    city: Mapped[Optional[str]] = mapped_column(Text(), nullable=True)
+    season: Mapped[Optional[str]] = mapped_column(Text(), nullable=True)
---
+++ backend/app/models/problemset.py
@@ -0,0 +1,91 @@
+"""
+    Módulo do model de problemas do Codeforces
+
+    A modelagem final dos problemas foi determinada da seguinte forma:
+    - Documentação oficial do Codeforces: https://codeforces.com/apiHelp
+    - A realidade vinda dos testes
+
+    Dado uma amostra de 11356 problemas, eis a frequência de cada campo:
+    - contestId = 11356/11356 (100,00%)
+    - problemsetName = 0/11356 (0,00%)
+    - index = 11356/11356 (100,00%)
+    - name = 11356/11356 (100,00%)
+    - type = 11356/11356 (100,00%)
+    - points = 7442/11356 (65,53%)
+    - rating = 11080/11356 (97,57%)
+    - tags = 11356/11356 (100,00%)
+
+    Apesar de problemsetName estar na documentação, não se aparenta estar sendo usado.
+    Mas mantenhamos compatibilidade com a documentação oficial, caso seja usado no futuro.
+
+    Além disso, outro objeto era o ProblemStatistics, que trazia o campo solvedCount, separado
+    do objeto Problem. O teste também comprovou que contestId, index e solvedCount estão 100% 
+    presentes, mas como forma de simplificação, vamos manter o campo solvedCount dentro de Problem.
+
+"""
+
+from typing import Optional, List
+import enum
+from sqlalchemy import String, Integer, Float, Enum
+from sqlalchemy import UniqueConstraint, ForeignKey
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+class ProblemsetBase(DeclarativeBase):
+    """ Base do banco de problemas do Codeforces """
+
+class ProblemType(enum.Enum):
+    """ Tipos de problemas """
+
+    PROGRAMMING = 'PROGRAMMING'
+    QUESTION = 'QUESTION'
+
+class Problem(ProblemsetBase):
+    """ Representa um problema do Codeforces """
+
+    __tablename__ = 'problem'
+    __table_args__ = (
+        UniqueConstraint(
+            'contestId', 'index'
+        ),
+    )
+    id: Mapped[int] = mapped_column(primary_key=True) # ID da linha
+    contest_id: Mapped[int] = mapped_column("contestId", Integer)
+    problemset_name: Mapped[str | None] = mapped_column("problemsetName", String(255))
+    index: Mapped[str] = mapped_column(String(10))
+    name: Mapped[str] = mapped_column(String(255))
+    type: Mapped[ProblemType] = mapped_column(Enum(ProblemType))
+    points: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    rating: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    solved_count: Mapped[int] = mapped_column("solvedCount", Integer, nullable=False)
+    tags: Mapped[List["Tag"]] = relationship(
+        secondary="problem_tag",
+        back_populates="problems"
+    )
+
+class ProblemTag(ProblemsetBase):
+    """ Associação entre problemas e tags """
+
+    __tablename__ = 'problem_tag'
+
+    problem_id: Mapped[int] = mapped_column(
+        "problemId", ForeignKey("problem.id"), primary_key=True
+    )
+    tag_id: Mapped[int] = mapped_column(
+        "tagId", ForeignKey("tag.id"), primary_key=True
+    )
+
+class Tag(ProblemsetBase):
+    """ Representa uma tag de problema (dp, implementation, etc) """
+
+    __tablename__ = 'tag'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+
+    problems: Mapped[List[Problem]] = relationship(
+        secondary="problem_tag",
+        back_populates="tags"
+    )
